039d3cb15a3273d10e6a270c7d17e2c4947a7818
Maxime22 <sabouretmaxime@gmail.com>
2021-06-15 12:14:53 +0200

    code climate

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -13,7 +13,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
-class UserController extends AbstractController{
+class UserController extends AbstractController
+{
 
     /**
      * @Route("/profile/{id}", name="profile")
@@ -21,35 +22,39 @@ class UserController extends AbstractController{
      */
     public function index(User $user, Request $request, TrickRepository $trickRepository, FileUploader $fileUploader): Response
     {
-        if($this->getUser()->getId() !== $user->getId()){
+        if ($this->getUser()->getId() !== $user->getId()) {
             return $this->redirectToRoute('home');
         }
 
         $form = $this->createForm(ProfileType::class, $user);
         $form->handleRequest($request);
 
-        $tricks = $trickRepository->findBy(['author'=>$this->getUser()->getId()]);
+        $tricks = $trickRepository->findBy(['author' => $this->getUser()->getId()]);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $em = $this->getDoctrine()->getManager();
 
-            $photoFile = $form->get('photoFile')->getData();
-            if ($photoFile) {
-                $oldFile = null;
-                if ($user->getPhoto() && $user->getPhoto() !== "avatar.jpeg") {
-                    $oldFile = new File($this->getParameter('userUpload_directory') . "/" . $user->getPhoto());
-                }
-                $newFilename = $fileUploader->upload($photoFile, $this->getParameter('userUpload_directory'), $oldFile);
-                $user->setPhoto($newFilename);
-            }
+            $this->uploadPhotoFile($form, $user, $fileUploader);
 
             $user->setUpdatedAt();
             $em->flush();
             $this->addFlash('notice', 'Profil sauvegardé');
-            return $this->redirectToRoute('profile', ['id'=>$user->getId()]);
+            return $this->redirectToRoute('profile', ['id' => $user->getId()]);
         }
 
-        return $this->render('user/index.html.twig', ['user' => $user,'form' => $form->createView(),'tricks' => $tricks]);
+        return $this->render('user/index.html.twig', ['user' => $user, 'form' => $form->createView(), 'tricks' => $tricks]);
     }
 
+    public function uploadPhotoFile($form, $user, $fileUploader)
+    {
+        $photoFile = $form->get('photoFile')->getData();
+        if ($photoFile) {
+            $oldFile = null;
+            if ($user->getPhoto() && $user->getPhoto() !== "avatar.jpeg") {
+                $oldFile = new File($this->getParameter('userUpload_directory') . "/" . $user->getPhoto());
+            }
+            $newFilename = $fileUploader->upload($photoFile, $this->getParameter('userUpload_directory'), $oldFile);
+            $user->setPhoto($newFilename);
+        }
+    }
 }
